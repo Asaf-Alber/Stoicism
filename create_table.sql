dcc273afc0ae0be6d89e419301ef35761f0bfa89
AsafAlber1 <asafalber@mail.tau.ac.il>
2024-12-24 00:02:00 +0200

    Delete create_table.sql

--- create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE questionnaire_responses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    age INT,
-    knowledge_level VARCHAR(50),
-    philosophers VARCHAR(255),
-    interest TEXT
-);
